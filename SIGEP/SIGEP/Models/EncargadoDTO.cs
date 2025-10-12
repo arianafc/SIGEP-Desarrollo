8b48295755e8cf1c8d7c1e033579e2bc01d0efa7
arianafc <123421110+arianafc@users.noreply.github.com>
2025-10-12 01:35:48 -0600

    Update modelo encargados

--- SIGEP/SIGEP/Models/EncargadoDTO.cs
+++ SIGEP/SIGEP/Models/EncargadoDTO.cs
@@ -11,7 +11,17 @@ namespace SIGEP.Models
         public string Nombre { get; set; }   
         public string Telefono { get; set; }
         public string Parentesco { get; set; }
-        public string Direccion { get; set; } 
+        public string LugarTrabajo { get; set; } 
         public string Ocupacion { get; set; }
+
+        public string Correo { get; set; }
+
+        public string Cedula { get; set; }
+        public DateTime FechaRegistro { get; set; }
+
+        public int IdEstado { get; set; }
+
+
+
     }
 }
