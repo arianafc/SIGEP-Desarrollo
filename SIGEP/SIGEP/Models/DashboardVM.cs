c88fc6560627ea7768801d23e42c27c12c02024c
arianafc <123421110+arianafc@users.noreply.github.com>
2025-11-08 13:09:01 -0600

    Modelo para el dashboard

---
+++ SIGEP/SIGEP/Models/DashboardVM.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SIGEP.Models
+{
+    public class DashboardVM
+    {
+        public int EstudiantesActivos { get; set; }
+
+        public int EmpresasRegistradas { get; set; }
+
+        public int EstudiantesConPracticasAsignadas { get; set; }
+
+        public int PracticasFinalizadas { get; set; }
+
+        public List<PracticaEstudianteViewModel> UltimasPracticasAsignadas { get; set; }
+
+
+
+    }
+}
